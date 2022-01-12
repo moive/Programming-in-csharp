a79aacea9238de2e56ac8cd57e44d07cfcc96c38
Moises Velasquez <mvelasquez@pictoric.com>
2022-01-12 18:46:43 -0500

    working with DateTime

--- Programming in csharp/Program.cs
+++ Programming in csharp/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+DateTime date = new DateTime(2021, 12, 30);
+DateTime dateHours = new DateTime(2020, 11, 6, 9, 30, 45);
+
+Console.WriteLine(date.ToString());
+Console.WriteLine(date.ToString("MM/dd/yyyy"));
+Console.WriteLine(date.ToString("dd-MM-yy"));
+Console.WriteLine(dateHours.ToString("dd-MM-yyyy hh:mm:ss"));
+
+Console.WriteLine(date.AddDays(45).ToString());
+
+Console.WriteLine(dateHours.DayOfWeek);
+Console.WriteLine(dateHours.Date);
+// get the difference of days
+Console.WriteLine(date.Subtract(dateHours).Days);
