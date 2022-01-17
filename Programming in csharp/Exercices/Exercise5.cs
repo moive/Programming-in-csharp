071252d0c807e55f5820badb3c208f659ed12181
Moises Velasquez <mvelasquez@pictoric.com>
2022-01-17 12:17:01 -0500

    Exercise 5

---
+++ Programming in csharp/Exercices/Exercise5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    internal class Exercise5
+    {
+        //Utilizando un iterador escribir en pantalla los números de  al 100 de dos en dos.
+        public Exercise5()
+        {
+            init();
+        }
+        public void init()
+        {
+            for (int i = 1; i <= 100; i=i+2)
+            {
+                Console.WriteLine($"{i}, {i+1}");
+            }
+        }
+    }
+}
--- Programming in csharp/Program.cs
+++ Programming in csharp/Program.cs
@@ -15,4 +15,4 @@ wdatetime.initializeDatetime();*/
 //Exercise1 exercise1 = new Exercise1();
 //Exercise2 exercise2 = new Exercise2();
 //Exercise3 exercise3 = new Exercise3();
-Exercise4 exercise4 = new Exercise4();
+Exercise5 exercise5 = new Exercise5();
