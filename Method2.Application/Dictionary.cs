2730a3f8bf39839df3dd89394bb3746e6771f147
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-11 23:21:54 -0500

    work with dictionaries

---
+++ Method2.Application/Dictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class Dictionary
+    {
+        public Dictionary()
+        {
+            Dictionary<string, int> nameAge = new Dictionary<string, int>();
+            nameAge.Add("Moises", 40);
+            nameAge.Add("Jorge", 45);
+            nameAge.Add("Maria", 45);
+
+            Console.WriteLine("Moises is {0} year old", nameAge["Moises"]);
+
+            if (nameAge.ContainsKey("itemNotExists"))
+            {
+                Console.WriteLine(nameAge["itemNotExists"]);
+            }
+
+            nameAge.Remove("Maria");
+
+            foreach (var keys in nameAge.Keys)
+            {
+                Console.WriteLine(keys);
+            }
+
+        }
+    }
+}
