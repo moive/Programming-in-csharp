42f5f2ccb0b788b023572d44175525d54592cf93
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-11 22:30:14 -0500

    generic class

---
+++ Method2.Application/FileGeneric.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class FileGeneric
+    {
+        public FileGeneric()
+        {
+            MyCollection<decimal, string> myCollection = new MyCollection<decimal, string>();
+            myCollection.Add(20);
+            myCollection.Add("Moises");
+
+            myCollection.GetAllItem();
+        }
+    }
+
+    class MyCollection<T,M>
+    {
+        public List<T> MyList { get; set; }
+
+        public MyCollection()
+        {
+            MyList = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            MyList.Add(item);
+        }
+
+        public void Add(M value)
+        {
+            //....
+        }
+
+        public void GetAllItem()
+        {
+            foreach (T item in MyList ) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
