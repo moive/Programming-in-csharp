4bdb56208b0c3fbe7675ff3f7c66a76e6d83d49b
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-11 19:45:08 -0500

    readonly and const

---
+++ Method2.Application/ReadonlyConst.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class ReadonlyConst
+    {
+        const double PI = 3.1415;
+        readonly double PI_2 = RepositoryValues.PI;
+
+        public ReadonlyConst()
+        {
+            Console.WriteLine("Value of const PI: {0}",PI);
+            Console.WriteLine("Value of readonly PI_2 {0}:", PI_2);
+        }
+    }
+
+    public static class RepositoryValues
+    {
+        public static double PI = 3.1415;
+    }
+}
