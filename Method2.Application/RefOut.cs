bfeafe4e74321fa914dd59d6b2518975e4507e52
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-11 20:27:18 -0500

    ref out

---
+++ Method2.Application/RefOut.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class RefOut
+    {
+        public RefOut()
+        {
+            var person = new Person() { Name = "Moises" };
+
+            // Type reference
+            Console.WriteLine("--Type of Reference--");
+            Console.WriteLine("Before the change");
+            Console.WriteLine(person.Name);
+            EditPerson(person, "Robert");
+            Console.WriteLine("After the change");
+            Console.WriteLine(person.Name);
+
+            Console.WriteLine("");
+
+            // Type of value
+            Console.WriteLine("--Type of value--");
+            Console.WriteLine("Before the change");
+            int age; // out is not necesary init value
+            //Console.WriteLine(age);
+            EditAge(out age, 14);
+            Console.WriteLine("After the change");
+            Console.WriteLine(age);
+        }
+
+        static void EditPerson(Person person, string newName) { 
+            person.Name = newName;
+        }
+
+        static void EditAge(out int currentAge, int newAge)
+        {
+            currentAge = newAge;
+        }
+    }
+}
