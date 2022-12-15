a8e01dd06823a6ff7d7d95da5da2c073c10af197
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-15 17:44:49 -0500

    first or default

--- Method3.Linq/Linq.cs
+++ Method3.Linq/Linq.cs
@@ -17,19 +17,22 @@ namespace Method3.Linq
         {
             List<int> numbers = new List<int>() { 3, 5, 7, 8, 2, -3, -100, 523, 6, 22 };
             //numbers.Reverse(1,8); //reverse 5, ..., 6
+
+            var elementFirst = numbers.FirstOrDefault(x => x % 2 == 0);
             //var result = numbers.Where((x, index) => index % 2 == 0).ToList();
             var result = numbers.Where(x => x > 0).OrderByDescending(x => x).ToList();
-
 
             List<Person> persons = new List<Person>() {
                 new Person(){Name = "María", Age = 99, Salary=5},
                 new Person(){Name = "Claudia", Age = 35, Salary=7},
-                new Person(){Name = "Mateo", Age = 17, Salary=10},
                 new Person(){Name = "Patricia", Age = 12, Salary=1},
+                new Person(){Name = "Mateo", Age = 17, Salary=10},
                 new Person(){Name = "Ana", Age = 22, Salary=5},
             };
 
-            var listPersons = persons.OrderBy(x => x.Salary).ThenBy(x=>x.Name).ToList();
+            var personUnderAge = persons.Where(x => x.Age < 18).OrderByDescending(x => x.Age).ToList().FirstOrDefault();
+
+            var listPersons = persons.OrderBy(x => x.Salary).ThenBy(x => x.Name).ToList();
 
         }
     }
