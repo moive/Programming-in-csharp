d371914723173e93c1dce4a566391a55c1827a91
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-14 19:27:45 -0500

    predicate example

---
+++ Method2.Application/PredicateExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class PredicateExample
+    {
+        public PredicateExample()
+        {
+            Predicate<Persona> predicate = IsOfAge;
+            var p = new Persona()
+            {
+                Name = "Test",
+                Age = 25
+            };
+
+            Console.WriteLine(predicate(p));
+        }
+
+        public bool IsOfAge(Persona person)
+        {
+            return person.Age >= 18;
+        }
+    }
+
+    public class Persona
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
