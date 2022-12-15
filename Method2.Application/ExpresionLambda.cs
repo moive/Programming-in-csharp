37683e20ef960c53b30727cb780e12b7dab5c847
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-14 21:08:31 -0500

    Expression lambda

---
+++ Method2.Application/ExpresionLambda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2.Application
+{
+    public class ExpresionLambda
+    {
+        public ExpresionLambda()
+        {
+            Func<int, int> duplicate = x => x * 2;
+            Func<int, int, int> multiply = (x, y) => x * y;
+
+            Console.WriteLine(duplicate(25));
+            Console.WriteLine(multiply(6,5));
+        }
+    }
+}
