71ad3ed01692a214fd3893a23f8a8f5406d6b7c1
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-16 17:14:30 -0500

    sqlConnectionString

--- Programming in csharp/Program.cs
+++ Programming in csharp/Program.cs
@@ -4,7 +4,16 @@ using Method3.Linq;
 using Operations;
 using Programming_in_csharp;
 using System.Configuration;
+using System.Data.SqlClient;
 
 var message = ConfigurationManager.AppSettings["message"];
 Console.WriteLine(message);
+
+var builder = new SqlConnectionStringBuilder();
+builder.DataSource = @"MVELASQUEZ";
+builder.InitialCatalog = "Prueba";
+builder.IntegratedSecurity = true;
+
+var connectionString = builder.ToString();
+
 Console.ReadKey();
